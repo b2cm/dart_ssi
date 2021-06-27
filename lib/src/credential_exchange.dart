6d30be28fa69e054aa45964c4c134bbbe50a3d08
Sarah Otto <otto3@hs-mittweida.de>
2021-06-27 14:17:08 +0200

    add classes for credential_exchange protocol

---
+++ lib/src/credential_exchange.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+class CredentialRequest {
+  List<dynamic>? credentialTypes;
+  Map<String, dynamic>? requiredProperties;
+  String? responseUrl;
+  String? challenge;
+  String? domain;
+  String type = 'CredentialRequest';
+  String acceptType = 'CredentialResponse';
+  String vpType = 'VerifiablePresentation';
+  String selectiveDisclosureType = 'HashedPlaintextCredential2021';
+
+  CredentialRequest(this.credentialTypes, this.responseUrl, this.challenge,
+      [this.requiredProperties, this.domain]);
+
+  CredentialRequest.fromQuery(String query) {
+    Map<String, dynamic> json = jsonDecode(utf8.decode(base64Decode(query)));
+    if (json['type'] != type) throw FormatException('Unsupported Request Type');
+
+    responseUrl = json['responseUrl'];
+    challenge = json['challenge'];
+    if (json.containsKey('domain')) domain = json['domain'];
+
+    if (json['accept']['type'] != acceptType)
+      throw FormatException('Unsupported Accept Type');
+    if (json['accept']['verifiablePresentation']['type'] != vpType)
+      throw FormatException('Unsupported Verifiable Presentation Type');
+    credentialTypes =
+        json['accept']['verifiablePresentation']['credentialTypes'];
+
+    if (json['accept'].containsKey('selectiveDisclosure')) {
+      if (json['accept']['selectiveDisclosure']['type'] !=
+          selectiveDisclosureType)
+        throw FormatException('Unsupported Selective Disclosure Type');
+      requiredProperties =
+          json['accept']['selectiveDisclosure']['requiredProperties'];
+    }
+  }
+
+  String toQuery() {
+    var json = _toJson();
+    return base64UrlEncode(utf8.encode(jsonEncode(json)));
+  }
+
+  @override
+  String toString() {
+    return jsonEncode(_toJson());
+  }
+
+  Map<String, dynamic> _toJson() {
+    Map<String, dynamic> json = {};
+    Map<String, dynamic> accept = {};
+    Map<String, dynamic> vp = {};
+
+    vp['type'] = vpType;
+    vp['credentialTypes'] = credentialTypes;
+    accept['type'] = acceptType;
+    accept['verifiablePresentation'] = vp;
+
+    if (requiredProperties != null || requiredProperties!.length != 0) {
+      Map<String, dynamic> sd = {};
+      sd['type'] = selectiveDisclosureType;
+      sd['requiredProperties'] = requiredProperties;
+      accept['selectiveDisclosure'] = sd;
+    }
+
+    json['type'] = type;
+    json['accept'] = accept;
+    json['responseUrl'] = responseUrl;
+    json['challenge'] = challenge;
+    if (domain != null) {
+      json['domain'] = domain;
+    }
+
+    return json;
+  }
+}
+
+class CredentialResponse {
+  Map<String, dynamic>? verifiablePresentation;
+  List<dynamic>? plaintextCredentials;
+  String type = 'CredentialResponse';
+
+  CredentialResponse(this.verifiablePresentation, this.plaintextCredentials);
+
+  CredentialResponse.fromQuery(String query) {
+    Map<String, dynamic> json = jsonDecode(utf8.decode(base64Decode(query)));
+    if (json['type'] != type)
+      throw FormatException('Unsupported Response-Type');
+    verifiablePresentation = json['verifiablePresentation'];
+    if (json.containsKey('selectiveDisclosure'))
+      plaintextCredentials =
+          json['selectiveDisclosure']['plaintextCredentials'];
+    else
+      plaintextCredentials = [];
+  }
+
+  String toQuery() {
+    var json = _toJson();
+    return base64UrlEncode(utf8.encode(jsonEncode(json)));
+  }
+
+  Map<String, dynamic> _toJson() {
+    Map<String, dynamic> json = {};
+    Map<String, dynamic> selectiveDisclosure = {};
+    json['type'] = type;
+    json['verifiablePresentation'] = verifiablePresentation;
+    selectiveDisclosure['plaintextCredentials'] = plaintextCredentials;
+    json['selectiveDisclosure'] = selectiveDisclosure;
+    return json;
+  }
+
+  @override
+  String toString() {
+    return jsonEncode(_toJson());
+  }
+}
